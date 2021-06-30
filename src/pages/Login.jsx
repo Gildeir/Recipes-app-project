3e7c241d2a3ff47a2dd47acaf792f06b4a5c9523
Daniel <darkdanielr@gmail.com>
2021-06-30 14:33:15 -0300

    corrigido6

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,6 +31,7 @@ function Login() {
 
   const RedirectAndLocal = () => {
     localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
     localStorage.setItem('user', JSON.stringify({ email }));
   };
 
