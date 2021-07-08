4ba27cd99808bff5c534b765eb589569e0f3df2c
RafaMaths <rmathscoach@gmail.com>
2021-07-08 15:30:29 -0300

    begin progress Drink page

--- src/pages/ProgressoBebidas.jsx
+++ src/pages/ProgressoBebidas.jsx
@@ -1,12 +1,12 @@
 import React/* , { useState, useEffect, useContext } */ from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import ContextRecipe from '../provider/ContextRecipe';
 
 function ProgressoBebidas(/* props */) {
   // const { match: { params: { id } } } = props;
   // const { ApiIdDetalhe, funcHeartColor, heartColor } = useContext(ContextRecipe);
   // passar ApiIdDetalhe, id como parametros para funcHeartColor no onclick do coração de favoritar
-  // apidetalhe é o item que tem que renderizar na tela
+  // apidetalhe é o item que tem que renderizar na tela .
   return (
     <p>olá</p>
   );
